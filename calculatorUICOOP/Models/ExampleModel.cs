d25e635a9bf40fd6a6f9da79ec8a6473085831bf
Nicholas M Martin <nmmart04@louisville.edu>
2020-11-03 13:43:43 -0500

    Setup MVVM

---
+++ calculatorUICOOP/Models/ExampleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculatorUICOOP.Models
+{
+    public class ExampleModel
+    {
+        public string SomeValue { get; set; } = "Moshi ";
+
+        public ExampleModel()
+        {
+            SomeValue += SomeValue;
+        }
+    }
+}
