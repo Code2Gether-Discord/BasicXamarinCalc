ae0f48e5b751a534effecea146735790b4ad2fc6
chrisK00 <72318023+chrisK00@users.noreply.github.com>
2020-11-15 06:15:53 +0100

    implemented number_clicked to show up on display and clear display (#28)
    * implemented number_clicked to show up on display and clear display
    
    * converted string to decimal in order to remove leading zeros
    
    * trying mvvm
    
    * not working
    
    * what am i doing
    
    * mvvm
    
    * data binding working
    
    * updated showplusondisplay
    
    * Update MainPageViewModel.cs
    
    Removed + for now because im unsure what would be the easiest way to handle this+all other op
    
    * Remove event handlers from code-behind, replace w/ command in ViewModel for number input.
    
    * Input commands for all numbers, operators etc added. DeleteLastInput implemented function. Removed methods in view.cs
    
    * Update MainPageViewModel.cs
    
    Spelling error divide method fixed
    
    * Fixed duplicates
    
    Co-authored-by: Ari <DonSagiv@users.noreply.github.com>
    Co-authored-by: Nicholas M Martin <nmmart04@louisville.edu>

--- calculatorUICOOP/ViewModels/MainPageViewModel.cs
+++ calculatorUICOOP/ViewModels/MainPageViewModel.cs
@@ -1,24 +1,128 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace calculatorUICOOP.ViewModels
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : INotifyPropertyChanged
     {
-        #region Fields and Properties
-        // TODO
+        #region Fields 
+        private string _displayContent;
         #endregion
 
-        #region Constructors
+        #region Properties
+        public string DisplayContent
+        {
+            get => _displayContent;
+            set
+            {
+                if (_displayContent == value)
+                {
+                    return;
+                }
+                _displayContent = value;
+                // Send a "push" notification to the UI letting it know the DisplayContent has changed.
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand NumberInputCommand { get; set; }
+        public ICommand ClearInputCommand { get; set; }
+        public ICommand DeleteInputCommand { get; set; }
+        public ICommand DivideInputCommand { get; set; }
+        public ICommand MultiplyInputCommand { get; set; }
+        public ICommand PlusInputCommand { get; set; }
+        public ICommand MinusInputCommand { get; set; }
+        public ICommand RemainderInputCommand { get; set; }
+        public ICommand DecimalInputCommand { get; set; }
+        public ICommand EqualsInputCommands { get; set; }
+
+        #endregion
+
+        #region Delegates
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Constructor
         public MainPageViewModel()
         {
-            // TODO
+            NumberInputCommand = new Command<string>(ShowNumberOnDisplay);
+            ClearInputCommand = new Command(ClearScreen);
+            DeleteInputCommand = new Command(DeleteLastInput);
+            DivideInputCommand = new Command<string>(ShowDivideOnDisplay);
+            MultiplyInputCommand = new Command<string>(ShowMultiplyOnDisplay);
+            PlusInputCommand = new Command<string>(ShowPlusOnDisplay);
+            MinusInputCommand = new Command<string>(ShowMinusOnDisplay);
+            RemainderInputCommand = new Command<string>(ShowRemainderOnDisplay);
+            DecimalInputCommand = new Command<string>(ShowDecimalOnDisplay);
+            EqualsInputCommands = new Command(Equals);
         }
         #endregion
 
         #region Methods
-        // TODO
-        #endregion
+        private void OnPropertyChanged([CallerMemberName] string text = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(text));
+        }
+
+        public void ShowNumberOnDisplay(string textToDisplay)
+        {
+            DisplayContent += textToDisplay;
+            var removedLeadingZeros = decimal.Parse(DisplayContent);
+            DisplayContent = removedLeadingZeros.ToString();
+        }
+
+        public void ClearScreen()
+        {
+            DisplayContent = "0";
+        }
+
+        public void ShowPlusOnDisplay(string plus)
+        {
+
+        }
+
+        public void ShowMultiplyOnDisplay(string multiply)
+        {
+
+        }
+
+        public void ShowMinusOnDisplay(string minus)
+        {
+
+        }
+
+        public void DeleteLastInput()
+        {
+            if (DisplayContent != "")
+            {
+                DisplayContent = DisplayContent.Remove(DisplayContent.Length - 1);
+            }
+           
+        }
+
+        public void ShowRemainderOnDisplay(string remainder)
+        {
+
+        }
+
+        public void ShowDecimalOnDisplay(string decimalDot)
+        {
+
+        }
+
+        public void Equals()
+        {
+
+        }
+
+        public void ShowDivideOnDisplay(string divide)
+        {
+
+        }
+        #endregion 
     }
 }
--- calculatorUICOOP/Views/MainPage.xaml.cs
+++ calculatorUICOOP/Views/MainPage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using calculatorUICOOP.ViewModels;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace calculatorUICOOP
@@ -13,58 +9,22 @@ namespace calculatorUICOOP
     public partial class MainPage : ContentPage
     {
         #region Fields and Properties
-        MainPageViewModel vm { get; set; }
-        #endregion
+
+        private MainPageViewModel vm { get; set; }
+
+        #endregion Fields and Properties
 
         #region Constructor
+
         public MainPage()
         {
             InitializeComponent();
             vm = new MainPageViewModel();
+            BindingContext = vm;
         }
-        #endregion
 
+        #endregion Constructor
+      
         
-
-        private void Number_Clicked(object sender, EventArgs e)
-        {
-
-        }
-        private void Equals_Clicked(object sender, EventArgs e)
-        {
-
-        }      
-        private void Clear_Clicked(object sender, EventArgs e)
-        {
-
-        }
-        private void Decimal_Clicked(object sender, EventArgs e)
-        {
-
-        }
-        private void Remainder_Clicked(object sender, EventArgs e)
-        {
-
-        }
-        private void Minus_Clicked(object sender, EventArgs e)
-        {
-
-        }
-        private void Plus_Clicked(object sender, EventArgs e)
-        {
-
-        }
-        private void Divide_Clicked(object sender, EventArgs e)
-        {
-
-        }
-        private void Multiply_Clicked(object sender, EventArgs e)
-        {
-
-        }
-        private void Delete_Clicked(object sender, EventArgs e)
-        {
-
-        }
     }
 }
