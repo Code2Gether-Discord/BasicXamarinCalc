13c1e810c8e09fc6966d9f3fb73187b5aade37a0
Nicholas M Martin <nmmart04@louisville.edu>
2020-11-17 14:06:19 -0500

    Cleaned up some attributes

--- calculatorUICOOP.Tests/MathLogicTests.cs
+++ calculatorUICOOP.Tests/MathLogicTests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using calculatorUICOOP.Models;
-using calculatorUICOOP.ViewModels;
 using NUnit.Framework;
 
 namespace calculatorUICOOP.Tests
@@ -15,26 +14,22 @@ namespace calculatorUICOOP.Tests
         
         }
 
-        [Test]
         [TestCase(5,4,1)]
         [TestCase(0, 5,-5)]
         public void AddTest(double expected, double x, double y) =>
             Assert.AreEqual(expected, MathLogic.Add(x, y));
 
-        [Test]
         [TestCase(3, 4, 1)]
         [TestCase(10, 5, -5)]
         public void SubtractTest(double expected, double x, double y) =>
             Assert.AreEqual(expected, MathLogic.Subtract(x, y));
 
-        [Test]
         [TestCase(10, 5, 2)]
         [TestCase(-10, 5, -2)]
         [TestCase(10, -5, -2)]
         public void MultiplyTest(double expected, double x, double y) =>
             Assert.AreEqual(expected, MathLogic.Multiply(x, y));
 
-        [Test]
         [TestCase(10, 100, 10)]
         [TestCase(1, 0, 0, true)]
         [TestCase(0, 5, 0, true)]
