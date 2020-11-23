c948fe0a9171e5529074d3f4305631d0ee9742f7
itian <kristianne.nerona@gmail.com>
2020-11-23 11:57:00 -0800

    Initial commit for the test project

---
+++ calculatorUICOOP.Test/MainPageViewModelShould.cs
@@ -0,0 +1,76 @@
+﻿using calculatorUICOOP.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Windows.Input;
+
+namespace calculatorUICOOP.Test
+{
+    public class MainPageViewModelShould
+    {
+        private readonly MainPageViewModel _viewModel;
+        private readonly ICommand _clearInputCommand;
+        private readonly ICommand _numberInputCommand;
+        private readonly ICommand _decimalCommand;
+        private readonly ICommand _equalsCommand;
+
+        private Action InvokeClearInputCommand;
+        private Action<string> InvokeNumberCommand;
+        private Action InvokeDecimalCommand;
+        private Action InvokeEqualsCommand;
+
+        public MainPageViewModelShould()
+        {
+            _viewModel = new MainPageViewModel();
+
+            _clearInputCommand = _viewModel.ClearInputCommand;
+            InvokeClearInputCommand = () => _clearInputCommand.Execute("CE");
+
+            _numberInputCommand = _viewModel.NumberInputCommand;
+            InvokeNumberCommand = (number) => _numberInputCommand.Execute(number);
+
+            _decimalCommand = _viewModel.DecimalInputCommand;
+            InvokeDecimalCommand = () => _decimalCommand.Execute(".");
+
+            _equalsCommand = _viewModel.EqualsInputCommand;
+            InvokeEqualsCommand = () => _equalsCommand.Execute("=");
+        }
+
+        private void SetTheInputValue(string[] values)
+        {
+            foreach (var value in values)
+            {
+                if (value == ".")
+                    InvokeDecimalCommand();
+                else
+                    InvokeNumberCommand(value);
+            }
+                
+        }
+
+        [Test]
+        public void DisplayZeroWhenCEIsPressed()
+        {
+            InvokeClearInputCommand();
+            Assert.AreEqual("0", _viewModel.DisplayContent);
+        }
+
+        [TestCase("56", new string[] { "2", "6" }, new string[] { "3", "0" })]
+        [TestCase("3", new string[] { "1" }, new string[] { "2" })]
+        [TestCase("2.4", new string[] { "1", ".", "1" }, new string[] { "1", ".", "3" })]
+        public void Add(string expected, string[] val1, string[] val2)
+        {
+            SetTheInputValue(val1);    
+
+            ICommand cmdForOper = _viewModel.PlusInputCommand;
+            cmdForOper.Execute("+");
+
+            SetTheInputValue(val2);
+
+            InvokeEqualsCommand();
+
+            Assert.AreEqual(expected, _viewModel.DisplayContent);
+
+            InvokeClearInputCommand();
+        }
+    }
+}
---
+++ calculatorUICOOP.Test/MathLogicTest.cs
@@ -0,0 +1,45 @@
+using calculatorUICOOP.Models;
+using NUnit.Framework;
+
+namespace calculatorUICOOP.Test
+{
+    public class MathLogicTest
+    {
+        const double Delta = 0.00000001d;
+
+        [TestCase(56, 26, 30)]
+        [TestCase(1, -1, 2)]
+        [TestCase(2.4d, 1.1, 1.3)]
+        public void Add(double expected, double val1, double val2) => Assert.AreEqual(expected, MathLogic.Add(val1, val2), Delta);
+
+        [TestCase(4, 30, 26)]
+        [TestCase(0, -2, -2)]
+        [TestCase(-4, -2, 2)]
+        [TestCase(3.5, 5.8, 2.3)]
+        public void Subtract(double expected, double val1, double val2) => Assert.AreEqual(expected, MathLogic.Subtract(val1, val2));
+
+        [TestCase(27, 3, 9)]
+        [TestCase(0, 0, 0)]
+        [TestCase(0, 0, 9)]
+        [TestCase(6.25, 2.5, 2.5)]
+        [TestCase(-1, 1, -1)]
+        [TestCase(1, -1, -1)]
+        public void Multiply(double expected, double val1, double val2) => Assert.AreEqual(expected, MathLogic.Multiply(val1, val2));
+
+        [TestCase(1, 1, 1)]
+        [TestCase(0, 0, 9)]
+        [TestCase(-9, 9, -1)]
+        [TestCase(.5, 2.5, 5)]
+        [TestCase(2, .22, .11)]
+        public void Divide(double expected, double val1, double val2)
+        {
+            //Condition for 0 as divisor?
+            Assert.AreEqual(expected, MathLogic.Divide(val1, val2));
+        }
+
+        [TestCase(-.01, -1)]
+        [TestCase(100, 10000)]
+        [TestCase(0.0025, 0.25)]
+        public void Percentage(double expected, double val) => Assert.AreEqual(expected, MathLogic.ConvertToPercent(val));
+    }
+}
