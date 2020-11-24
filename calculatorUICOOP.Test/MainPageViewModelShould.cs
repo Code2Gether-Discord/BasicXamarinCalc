d3230fea8345f8158de052dbf6ad8781e11df3a0
itian <kristianne.nerona@gmail.com>
2020-11-23 19:06:26 -0800

    Updated initial commit of test projects

--- calculatorUICOOP.Test/MainPageViewModelShould.cs
+++ calculatorUICOOP.Test/MainPageViewModelShould.cs
@@ -1,76 +1,132 @@
 ﻿using calculatorUICOOP.ViewModels;
 using NUnit.Framework;
-using System;
-using System.Windows.Input;
 
 namespace calculatorUICOOP.Test
 {
     public class MainPageViewModelShould
     {
         private readonly MainPageViewModel _viewModel;
-        private readonly ICommand _clearInputCommand;
-        private readonly ICommand _numberInputCommand;
-        private readonly ICommand _decimalCommand;
-        private readonly ICommand _equalsCommand;
-
-        private Action InvokeClearInputCommand;
-        private Action<string> InvokeNumberCommand;
-        private Action InvokeDecimalCommand;
-        private Action InvokeEqualsCommand;
 
         public MainPageViewModelShould()
         {
             _viewModel = new MainPageViewModel();
-
-            _clearInputCommand = _viewModel.ClearInputCommand;
-            InvokeClearInputCommand = () => _clearInputCommand.Execute("CE");
-
-            _numberInputCommand = _viewModel.NumberInputCommand;
-            InvokeNumberCommand = (number) => _numberInputCommand.Execute(number);
-
-            _decimalCommand = _viewModel.DecimalInputCommand;
-            InvokeDecimalCommand = () => _decimalCommand.Execute(".");
-
-            _equalsCommand = _viewModel.EqualsInputCommand;
-            InvokeEqualsCommand = () => _equalsCommand.Execute("=");
         }
 
-        private void SetTheInputValue(string[] values)
+        [TestCase("5", "0005")]
+        [TestCase("5", "05")]
+        [TestCase("0.005", "0.005")]
+        [TestCase("50", "50")]
+        [TestCase("50.0", "50.0")]
+        [TestCase("50.0000000005", "50.0000000005")]
+        public void ShowNumberOnDisplay(string expected, string input)
         {
-            foreach (var value in values)
+            _viewModel.ClearScreen();
+            _viewModel.ShowNumberOnDisplay(input);
+            var actual = _viewModel.DisplayContent;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("0", "5")]
+        [TestCase("0", "55.01")]
+        [TestCase("0", "0")]
+        [TestCase("0", "max")]
+        [TestCase("0", "min")]
+        public void ClearScreenTest(string expected, string input)
+        {
+            switch (input)
             {
-                if (value == ".")
-                    InvokeDecimalCommand();
-                else
-                    InvokeNumberCommand(value);
+                case "max":
+                    input = decimal.MaxValue.ToString();
+                    break;
+                case "min":
+                    input = decimal.MinValue.ToString();
+                    break;
             }
-                
+
+            _viewModel.DisplayContent = input;
+            _viewModel.ClearScreen();
+            var actual = _viewModel.DisplayContent;
+            Assert.AreEqual(expected, actual);
         }
 
-        [Test]
-        public void DisplayZeroWhenCEIsPressed()
+        [TestCase("50", "500")]
+        [TestCase("5.55", "5.555")]
+        [TestCase("55.0", "55.01")]
+        [TestCase("0", "1")]
+        [TestCase("0", "5")]
+        public void DeleteLastInputTest(string expected, string input)
         {
-            InvokeClearInputCommand();
-            Assert.AreEqual("0", _viewModel.DisplayContent);
+            _viewModel.DisplayContent = input;
+            _viewModel.DeleteLastInput();
+            string actual = _viewModel.DisplayContent;
+            Assert.AreEqual(expected, actual);
         }
 
-        [TestCase("56", new string[] { "2", "6" }, new string[] { "3", "0" })]
-        [TestCase("3", new string[] { "1" }, new string[] { "2" })]
-        [TestCase("2.4", new string[] { "1", ".", "1" }, new string[] { "1", ".", "3" })]
-        public void Add(string expected, string[] val1, string[] val2)
+
+        [TestCase("56", "26", "30")]
+        [TestCase("3", "1", "2")]
+        [TestCase("2.4", "1.1", "1.3")]
+        public void Add(string expected, string val1, string val2)
         {
-            SetTheInputValue(val1);    
+            _viewModel.DisplayContent = val1;
+            _viewModel.ShowPlusOnDisplay("+");
+            _viewModel.DisplayContent = val2;
+            _viewModel.Equals();
 
-            ICommand cmdForOper = _viewModel.PlusInputCommand;
-            cmdForOper.Execute("+");
+            string actual = _viewModel.DisplayContent;
 
-            SetTheInputValue(val2);
+            Assert.AreEqual(expected, actual);
+        }
 
-            InvokeEqualsCommand();
+        [TestCase("4", "30", "26")]
+        [TestCase("0", "-2", "-2")]
+        [TestCase("-4", "-2", "2")]
+        [TestCase("3.5", "5.8", "2.3")]
+        public void Subtract(string expected, string val1, string val2)
+        {
+            _viewModel.DisplayContent = val1;
+            _viewModel.ShowMinusOnDisplay("-");
+            _viewModel.DisplayContent = val2;
+            _viewModel.Equals();
 
-            Assert.AreEqual(expected, _viewModel.DisplayContent);
+            string actual = _viewModel.DisplayContent;
 
-            InvokeClearInputCommand();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("27", "3", "9")]
+        [TestCase("0", "0", "0")]
+        [TestCase("0", "0", "9")]
+        [TestCase("6.25", "2.5", "2.5")]
+        [TestCase("-1", "1", "-1")]
+        [TestCase("1", "-1", "-1")]
+        public void Multiply(string expected, string val1, string val2)
+        {
+            _viewModel.DisplayContent = val1;
+            _viewModel.ShowMultiplyOnDisplay("*");
+            _viewModel.DisplayContent = val2;
+            _viewModel.Equals();
+
+            string actual = _viewModel.DisplayContent;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1", "1", "1")]
+        [TestCase("0", "0", "9")]
+        [TestCase("-9", "9", "-1")]
+        [TestCase("0.5", "2.5", "5")]
+        [TestCase("2", ".22", ".11")]
+        public void Division(string expected, string val1, string val2)
+        {
+            _viewModel.DisplayContent = val1;
+            _viewModel.ShowDivideOnDisplay("/");
+            _viewModel.DisplayContent = val2;
+            _viewModel.Equals();
+
+            string actual = _viewModel.DisplayContent;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- calculatorUICOOP.Test/MathLogicTest.cs
+++ calculatorUICOOP.Test/MathLogicTest.cs
@@ -10,13 +10,15 @@ namespace calculatorUICOOP.Test
         [TestCase(56, 26, 30)]
         [TestCase(1, -1, 2)]
         [TestCase(2.4d, 1.1, 1.3)]
-        public void Add(double expected, double val1, double val2) => Assert.AreEqual(expected, MathLogic.Add(val1, val2), Delta);
+        public void Add(double expected, double val1, double val2)
+            => Assert.AreEqual(expected, MathLogic.Add(val1, val2), Delta);
 
         [TestCase(4, 30, 26)]
         [TestCase(0, -2, -2)]
         [TestCase(-4, -2, 2)]
         [TestCase(3.5, 5.8, 2.3)]
-        public void Subtract(double expected, double val1, double val2) => Assert.AreEqual(expected, MathLogic.Subtract(val1, val2));
+        public void Subtract(double expected, double val1, double val2)
+            => Assert.AreEqual(expected, MathLogic.Subtract(val1, val2));
 
         [TestCase(27, 3, 9)]
         [TestCase(0, 0, 0)]
@@ -24,7 +26,8 @@ namespace calculatorUICOOP.Test
         [TestCase(6.25, 2.5, 2.5)]
         [TestCase(-1, 1, -1)]
         [TestCase(1, -1, -1)]
-        public void Multiply(double expected, double val1, double val2) => Assert.AreEqual(expected, MathLogic.Multiply(val1, val2));
+        public void Multiply(double expected, double val1, double val2)
+            => Assert.AreEqual(expected, MathLogic.Multiply(val1, val2));
 
         [TestCase(1, 1, 1)]
         [TestCase(0, 0, 9)]
@@ -40,6 +43,7 @@ namespace calculatorUICOOP.Test
         [TestCase(-.01, -1)]
         [TestCase(100, 10000)]
         [TestCase(0.0025, 0.25)]
-        public void Percentage(double expected, double val) => Assert.AreEqual(expected, MathLogic.ConvertToPercent(val));
+        public void Percentage(double expected, double val)
+            => Assert.AreEqual(expected, MathLogic.ConvertToPercent(val));
     }
 }
