c318eb7ad0215b9061f2ee364cf2c1939f4d64f3
Jheam Storch Ross <58143641+srjheam@users.noreply.github.com>
2020-12-20 15:38:52 -0300

    Added first implementation of a logger

--- calculatorUICOOP.Android/MainActivity.cs
+++ calculatorUICOOP.Android/MainActivity.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using calculatorUICOOP.Droid.Helpers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Runtime;
 
 namespace calculatorUICOOP.Droid
 {
     [Activity(Label = "calculatorUICOOP", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        private Logger _logger;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -22,6 +25,11 @@ namespace calculatorUICOOP.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            // Handling uncaught application exceptions
+            _logger = new Logger();
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -29,5 +37,17 @@ namespace calculatorUICOOP.Droid
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+        #region Handling uncaught application exceptions
+        private void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            var exception = unhandledExceptionEventArgs.ExceptionObject as Exception;
+            _logger.Log(LogLevel.Error, exception);
+        }
+        private void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs)
+        {
+            foreach (var exception in unobservedTaskExceptionEventArgs.Exception.InnerExceptions)
+                _logger.Log(LogLevel.Error, exception);
+        }
+        #endregion
     }
 }
